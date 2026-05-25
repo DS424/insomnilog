848f4a70fc55bfd5349c5fcf61971d07afc85729
Dwayne Steinke <DS424@d-steinke.de>
2026-05-25 12:10:01 +0200

    docs: Add chapter `Configuring the formatter`
    This change adds a short chapter with an example to the
    docs, how the formatter of the `ConsoleSink` can
    be used.
    
    commit-id:fd885bd0

--- insomnilog/build.rs
+++ insomnilog/build.rs
@@ -10,6 +10,7 @@ fn main() {
     out.push_str(&resolve_includes("docs/core_concepts.md"));
     out.push_str(&resolve_includes("docs/architecture.md"));
     out.push_str(&resolve_includes("docs/preallocating.md"));
+    out.push_str(&resolve_includes("docs/formatter.md"));
     out.push_str(&mermaid_script());
     std::fs::write(dest, out).unwrap();
 
